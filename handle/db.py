cc8dcbae6d50cb9c54caffc1e414f8a9bcc760dc
gyj <125370136@qq.com>
2017-04-21 17:48:11 +0800

    add

--- handle/db.py
+++ handle/db.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+
 import  uuid, functools, threading, logging, times
 
 # 数据库引擎对象：sd
@@ -54,6 +57,118 @@ class _LasyConnection(object):
             self.connection = None
             logging.info('close connection <%s>...' % hex(id(connection)))
             connection.close()
+
+def create_engine(user, password, database, host='127.0.0.1', port=3306, **kw):
+    import MySQLdb.connections
+    global engine
+    if engine is not None:
+        raise DBError('Engine is already initialized.')
+    params = dict(user=user, password=password, database=database, host=host, port=port)
+    defaults = dict(use_unicode=True, charset='utf8', collation='utf8_general_ci', autocommit=False)
+    for k, v in defaults.iteritems():
+        params[k] = kw.pop(k, v)
+        params.update(kw)
+        params['buffered'] = True
+        engine = _Engine(lambda: MySQLdb.Connect(**params))
+            # test connection...
+        logging.info('Init mysql engine <%s> ok.' % hex(id(engine)))
+
+class DBError(Exception):
+    pass
+
+class MultiColumnsError(DBError):
+    pass
+
+class _ConnectionCtx(object):
+    def __enter__(self):
+        global _db_ctx
+        self.should_cleanup = False
+        if not _db_ctx.is_init():
+            _db_ctx.init()
+            self.should_cleanup = True
+            return self
+
+    def __exit__(self, exctype, excvalue, traceback):
+        global _db_ctx
+        if self.should_cleanup:
+            _db_ctx.cleanup()
+
+def connection():
+    '''
+        Return _ConnectionCtx object that can be used by 'with' statement:
+        with connection():
+            pass
+    '''
+    return _ConnectionCtx()
+
+
+def with_connection(func):
+    '''
+    Decorator for reuse connection.
+    用来重用的装饰器
+    @with_connection
+    def foo(*args, **kw):
+        f1()
+        f2()
+        f3()
+    '''
+    @functools.wraps(func)
+    def _wrapper(*args, **kw):
+        with _ConnectionCtx():
+            return func(*args, **kw)
+    return _wrapper
+
+
+class _TransactionCtx(object):
+    '''
+    _TransactionCtx object that can handle transactions.
+    处理操作的对象
+    with _TransactionCtx():
+        pass
+    '''
+
+    def __enter__(self):
+        global _db_ctx
+        self.should_close_conn = False
+        if not _db_ctx.is_init():
+            # needs open a connection first:
+            _db_ctx.init()
+            self.should_close_conn = True
+        _db_ctx.transactions = _db_ctx.transactions + 1
+        logging.info('begin transaction...' if _db_ctx.transactions==1 else 'join current transaction...')
+        return self
+
+    def __exit__(self, exctype, excvalue, traceback):
+        global _db_ctx
+        _db_ctx.transactions = _db_ctx.transactions - 1
+        try:
+            if _db_ctx.transactions==0:
+                if exctype is None:
+                    self.commit()
+                else:
+                    self.rollback()
+        finally:
+            if self.should_close_conn:
+                _db_ctx.cleanup()
+
+    def commit(self):
+        global _db_ctx
+        logging.info('commit transaction...')
+        try:
+            _db_ctx.connection.commit()
+            logging.info('commit ok.')
+        except:
+            logging.warning('commit failed. try rollback...')
+            _db_ctx.connection.rollback()
+            logging.warning('rollback ok.')
+            raise
+
+    def rollback(self):
+        global _db_ctx
+        logging.warning('rollback transaction...')
+        _db_ctx.connection.rollback()
+        logging.info('rollback ok.')
+
 
 # ORM
 class Dict(dict):
